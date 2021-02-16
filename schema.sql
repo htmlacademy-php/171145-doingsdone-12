073cf18095499fc4f420db96cdb77fc52726afe6
Amaria-desveloper (Maria Aleksandrova) <amaria.desveloper@gmail.com>
2021-02-16 16:11:46 +0300

    create DB Doingsdone

--- schema.sql
+++ schema.sql
@@ -1,36 +1,36 @@
-CREATE DATABASE doingsdone
+CREATE DATABASE Doingsdone
   DEFAULT CHARACTER SET utf8
   DEFAULT COLLATE utf8_general_ci;
 
-USE doingsdone;
+USE Doingsdone;
 
-CREATE TABLE users (
-  user_id INT unsigned AUTO_INCREMENT PRIMARY KEY NOT NULL,
-  date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  email VARCHAR(128) UNIQUE NOT NULL,
-  name VARCHAR(60) NOT NULL,
-  password VARCHAR(64) NOT NULL,
+CREATE TABLE Users (
+  Id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  Date_reg TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  Email VARCHAR(128) UNIQUE NOT NULL,
+  Name VARCHAR(60) NOT NULL,
+  Password VARCHAR(64) NOT NULL
+);
+
+CREATE TABLE Projects (
+  Id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  Headline VARCHAR(255) NOT NULL,
+  User_id INT unsigned NOT NULL,
+  CONSTRAINT fk_project_user FOREIGN KEY (User_id) REFERENCES Users(Id)
+);
+
+CREATE TABLE Tasks (
+  Id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  Create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  Status TINYINT(1) DEFAULT 0 NOT NULL,
+  Title VARCHAR(255) NOT NULL,
+  File_path VARCHAR(255) NULL, 
+  Deadline_date TIMESTAMP NULL,
+  User_id INT unsigned NOT NULL,
+  CONSTRAINT fk_task_user FOREIGN KEY (User_id) REFERENCES Users(Id),
+  Project_id INT unsigned NOT NULL,
+  CONSTRAINT fk_task_project FOREIGN KEY (Project_id) REFERENCES Projects(Id)
 );
 
 
-CREATE TABLE tasks (
-  task_id INT unsigned AUTO_INCREMENT PRIMARY KEY NOT NULL,
-  add_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  status TINYINT(1) DEFAULT 0 NOT NULL,
-  title VARCHAR(255) NOT NULL,
-  -- file BLOB,
-  deadline_date TIMESTAMP NULL,
-  user_id INT NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users (user_id),
-  project_id INT NOT NULL,
-  FOREIGN KEY (project_id) REFERENCES projects (project_id)
-
-);
-
-CREATE TABLE projects (
-  project_id INT unsigned AUTO_INCREMENT PRIMARY KEY NOT NULL,
-  headline VARCHAR(255) UNIQUE NOT NULL,
-  user_id INT NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users (user_id)
-);
 
