82e6a4c611ad151ebcc5b7832743af322d284e51
Maria Aleksandrova <mail@aleksandrovamaria.ru>
2021-03-15 00:25:37 +0300

    stuck

--- index.php
+++ index.php
@@ -15,32 +15,28 @@ function set_db_connect(array $db_settings) {
     return mysqli_connect($db_settings['host'], $db_settings['user'], $db_settings['password'], $db_settings['name']);
 }
 
+
 $connect = set_db_connect($db);
 mysqli_set_charset($connect, "utf8");
 
-    if (!$connect) {
-        die('Ошибка подключения: ' . mysqli_connect_error() . mysqli_errno());
-    }
-    else {
-        echo 'it`s working';
-       /*  $sql_projects = "SELECT * FROM projects WHERE user_id = 1";
-        $sql_tasks = "SELECT * FROM tasks where user_id = 1";
-        $result_projects = mysqli_query($connect, $sql_projects);
-        $result_tasks= mysqli_query($connect, $sql_tasks);
+if (!$connect) {
+    die('Ошибка подключения: ' . mysqli_connect_error() . mysqli_errno());
+}
 
-        if ($result_projects) {
-            $projects = mysqli_fetch_all($result_projects, MYSQLI_ASSOC);
-        }
+$sql_get_projects = "SELECT * FROM projects where user_id = '1'";
+$result_projects = mysqli_query($connect, $sql_get_projects);
 
-        if ($result_tasks) {
-            $tasks = mysqli_fetch_all($result_tasks, MYSQLI_ASSOC);
-        } */
-    }
-        /* else {
-            $error = mysqli_error($connect);
-            print('ошибка' . $error);
-        }
-    }*/
+if(!$result_projects) {
+    $error - mysqli_error($connect);
+    print('Ошибка MySQL: ' . $error);
+}
+
+$projects = mysqli_fetch_all($result_projects, MYSQLI_ASSOC);
+
+
+
+//$sql_get_tasks = "SELECT * FROM tasks where user_id = '1'";
+
 
 //variables
 $user_name = 'Марти';
@@ -51,7 +47,7 @@ $show_complete_tasks = rand(0, 1);
 
 $page_content = include_template('main.php', [
     'projects' => $projects,
-    'tasks' => $tasks,
+    //'tasks' => $tasks,
     'show_complete_tasks' => $show_complete_tasks,
 ]);
 
@@ -59,7 +55,6 @@ $layout = include_template('layout.php', [
     'page_content' => $page_content,
     'user_name' => $user_name,
     'page_title' => $page_title,
-    'error' => $error
 ]);
 
 print $layout;
